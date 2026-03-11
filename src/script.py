0dcfd3037ca50ea8f39b9c3e682313a90035674e
yaroslav <yaroslav.miklush@gmail.com>
2026-03-11 14:23:36 -0700

    fix3

--- src/script.py
+++ src/script.py
@@ -22,11 +22,11 @@ conn.commit()
 
 # Замер БЕЗ индекса
 start = time.time()
-for _ in range(100):
+for _ in range(1000):
     cursor.execute("SELECT * FROM data_base1 WHERE Fare > 100")
     _ = cursor.fetchall()
 time_without = time.time() - start
-print(f"⏱ Без индекса (100 запросов): {time_without:.4f} сек")
+print(f" Без индекса (1000 запросов): {time_without:.4f} сек")
 
 # Создаем индекс
 cursor.execute("CREATE INDEX idx_fare ON data_base1(Fare)")
@@ -34,23 +34,23 @@ conn.commit()
 
 # Замер С индексом
 start = time.time()
-for _ in range(100):
+for _ in range(1000):
     cursor.execute("SELECT * FROM data_base1 WHERE Fare > 100")
     _ = cursor.fetchall()
 time_with = time.time() - start
-print(f"⏱ С индексом (100 запросов): {time_with:.4f} сек")
+print(f" С индексом (1000 запросов): {time_with:.4f} сек")
 
-print(f"\n🚀 Ускорение: {time_without / time_with:.2f}x")
+print(f"\n Ускорение: {time_without / time_with:.2f}x")
 
 
 df_raw.to_sql('data_base1', conn , if_exists='replace', index=False)
-print(" Данные загружены в SQL таблицу")
+print(" Данные загружены в sql таблицу")
 
 
 
 query="""
 SELECT (select avg(Fare) from data_base1) as average_price,Passengerid,Age,Fare,Survived from data_base1
-WHERE fare>70 
+WHERE fare>80 
 LIMIT 200
 """
 df=pd.read_sql_query(query,conn)
@@ -60,7 +60,7 @@ df['median_age'] = df['Age'].median()
 df['Is_Rich'] = (df['Fare'] > 100).astype(int)
 mode_fare=df['Fare'].mode()[0]
 df['Fare']=df['Fare'].fillna(mode_fare) #change 0 fare to most common fare
-# Корреляция между ценой билета и выживаемостью (NumPy)
+# Корреляция между ценой билета и выживаемостью 
 corr = np.corrcoef(df['Is_Rich'].values, df['Survived'].values)[0, 1]
 print(f"\nКорреляция Rich и Survived: {corr:.4f}")
 df.to_csv('cleaned_titanic.csv', index=False)
